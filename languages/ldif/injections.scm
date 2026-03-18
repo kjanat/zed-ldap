5e75fa31037e7d5fa559d39c2c846283fdd19617
Kaj Kowalski <info@kajkowalski.nl>
2026-03-18 15:09:55 +0100

    match OLC schema attrs case-insensitively

--- languages/ldif/injections.scm
+++ languages/ldif/injections.scm
@@ -1,5 +1,5 @@
 (attrval_spec
   (attribute_name) @_attr
   (value) @injection.content
-  (#match? @_attr "^olc(AttributeTypes|ObjectClasses|LdapSyntaxes|DitContentRules|MatchingRules|MatchingRuleUse|NameForms)$")
+  (#match? @_attr "(?i)^olc(attributetypes|objectclasses|ldapsyntaxes|ditcontentrules|matchingrules|matchingruleuse|nameforms)$")
   (#set! injection.language "ldap_schema"))
